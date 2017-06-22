88a18bbb0e4d8727f098e8be3841797074e6d35c
mellaniesoriano <msoriano808@gmail.com>
2017-06-21 20:44:39 -1000

    chirped 3 times

--- js/chirp.js
+++ js/chirp.js
@@ -1,6 +1,13 @@
 function chirp(n){
-  // FIX ME
+  if ( n === 1 ) {
+    return "chirp";
+  }
+
+  console.log(n);
+  return "chirp " + chirp(--n);
 }
+
+console.log(chirp(3));
 
 $(document).ready(function(){
   $("#result").html(chirp(3));
