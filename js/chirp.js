cb1afdffc7f77948278c51fbebdb089ba393bfe5
mellaniesoriano <msoriano808@gmail.com>
2017-06-22 18:49:38 -1000

    cleaned up whitespaces and console.logs

--- js/chirp.js
+++ js/chirp.js
@@ -2,12 +2,8 @@ function chirp(n){
   if ( n === 1 ) {
     return "chirp";
   }
-
-  console.log(n);
   return "chirp " + chirp(--n);
 }
-
-// chirp(3);
 
 $(document).ready(function(){
   $("#result").html(chirp(3));
